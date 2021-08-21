fa9f7fb5712d17b54022a13e2ee5282227cb7ca4
matsur <ru.far2014@gmail.com>
2021-08-21 14:16:33 +0500

    new: index page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const formInputs = document.querySelectorAll('.form-input')
+const indexBurger = document.querySelector('.index-burger')
+indexBurger.addEventListener('click', () => {
+  const upLayer = document.querySelector('.up-layer')
+  upLayer.classList.add('show-up-layer')
+  
+  const leftMenu = document.querySelector('.left-menu')
+  leftMenu.classList.add('show-left-menu')
+})
